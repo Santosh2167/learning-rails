5cbb10a76fdb8b9c89e20ca5aafcb05d9145bf41
Santosh2167 <santosh.poudyal@gmail.com>
2018-09-26 17:03:08 -0700

    linking folder with github

---
+++ array.rb
@@ -0,0 +1,30 @@
+# Don't use any array helper methods except for .each
+
+# This is the same as the "Is it in the array?" challenge with one improvement:
+
+# in_array_advanced takes an additional boolean argument 'strict'.
+
+# If strict is true, in_array should care about leTteR CasIng.
+
+# If strict is false, in_array should return true for any match.
+
+
+# in_array("HeLLo", ["hi", "howdy", "HeLLo"], true) should return true
+
+# in_array("DrAkE", ["jayz", "kanye", "drake"], false) should return true
+
+# in_array("DrAkE", ["jayz", "kanye", "yachty"], false) should return false
+
+
+def in_array_advanced (needle, haystack, strict)
+    # Your code here
+    puts needle
+    puts haystack
+    puts strict
+    
+    
+  
+  
+  end
+
+  in_array_advanced(needle,['hi','hello','bye'],flase)
