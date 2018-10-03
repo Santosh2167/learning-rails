e66c799add78004be320d4a7923c443a54905e2c
Santosh <santosh.poudyal@gmail.com>
2018-10-03 12:47:10 +1000

    linking mvc in Rails, Garet's lecture

---
+++ movie_directory_app/app/controllers/movies_controller.rb
@@ -0,0 +1,35 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :runtime)
+  end
+end
